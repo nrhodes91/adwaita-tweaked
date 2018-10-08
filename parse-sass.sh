e235c106edd5af237fe0a3f0046602e6b9803e31
Nick Rhodes <nrhodes91@gmail.com>
2018-10-08 06:07:16 +0100

    Add gnome-shell files and move parse-scss.sh to repo root directory

---
+++ parse-sass.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+if [ ! "$(which sassc 2> /dev/null)" ]; then
+   echo sassc needs to be installed to generate the css.
+   exit 1
+fi
+
+SASSC_OPT=( "-l" "-M" "-t" "expanded" )
+
+echo Generating GTK3 css...
+
+sassc "${SASSC_OPT[@]}" gtk-3.0/gtk-light.{scss,css}
+sassc "${SASSC_OPT[@]}" gtk-3.0/gtk-dark.{scss,css}
+
+echo Generating GnomeShell css...
+
+sassc "${SASSC_OPT[@]}" gnome-shell/gnome-shell.{scss,css}
