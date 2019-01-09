ef131667f25d5a0aa28991eb086538c55763ea1d
Nick Rhodes <nrhodes91@gmail.com>
2019-01-09 19:45:07 +0000

    Wrap echo in double quotes

--- parse-sass.sh
+++ parse-sass.sh
@@ -7,7 +7,7 @@ fi
 
 SASSC_OPT=( "-M" "-t" "compressed" )
 
-echo Generating GTK3 css...
+echo "Generating GTK3 css..."
 
 sassc "${SASSC_OPT[@]}" gtk-3.0/gtk-light.{scss,css}
 sassc "${SASSC_OPT[@]}" gtk-3.0/gtk-dark.{scss,css}
